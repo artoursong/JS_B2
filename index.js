af53ba3deff3ee76c1a38c763aed001dd7c4e1ac
artoursong <duy147862@gmail.com>
2022-11-09 22:18:18 +0700

    update select slide move left

--- index.js
+++ index.js
@@ -1,97 +1,151 @@
 var Slider = {
-    GLOBAL: {
-        imgCount: $(".image-slide-container img").length,
-        index: 0,
-    },
-    CONSTS: {},
-    SELECTORS: {
-        image_slide_container: ".image-slide-container",
-        prev_button: ".prev-button",
-        next_button: ".next-button",
-        img: ".image-slide-container img",
-        slide_select_container: ".slide-select-container",
-        select_button: ".slide-select-container .select-button",
-    },
-    init: function () {
-        Slider.renderContainer();
-        Slider.setUpEvent();
-        Slider.selectButtonEvent();
-    },
+  GLOBAL: {
+    imgCount: $(".image-slide-container img").length,
+    index: 0,
+  },
+  CONSTS: {},
+  SELECTORS: {
+    image_slide_container: ".image-slide-container",
+    prev_button: ".prev-button",
+    next_button: ".next-button",
+    img: ".image-slide-container img",
+    slide_select_container: ".slide-select-container",
+    select_button: ".slide-select-container .select-button",
+  },
+  init: function () {
+    Slider.renderContainer();
+    Slider.setUpEvent();
+    Slider.selectButtonEvent();
+  },
 
-
-    selectButtonEvent: function () {
-        $(Slider.SELECTORS.select_button).on("click",function() {
-            let selectIndex = $(this).attr("index");
-            if (selectIndex > Slider.GLOBAL.index) {
-                let subIndex = Number(selectIndex) - Number(Slider.GLOBAL.index)
-                for (let i = 0; i < subIndex; i++) {
-                    $(Slider.SELECTORS.image_slide_container).animate({left: - 748},700, function() {
-                        $(".image-slide-container img:first-child").appendTo($(Slider.SELECTORS.image_slide_container));
-                        $(Slider.SELECTORS.image_slide_container).css("left","");
-                    })
-                    $(Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`).removeClass("active");
-                    Slider.GLOBAL.index = Slider.GLOBAL.index >= Number(Slider.GLOBAL.imgCount)-1 ? 0 : Number(Slider.GLOBAL.index)+1;
-                    $(Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`).addClass("active");
-                }
+  selectButtonEvent: function () {
+    $(Slider.SELECTORS.select_button).on("click", function () {
+      let selectIndex = $(this).attr("index");
+      if (selectIndex > Slider.GLOBAL.index) {
+        let subIndex = Number(selectIndex) - Number(Slider.GLOBAL.index);
+        for (let i = 0; i < subIndex; i++) {
+          $(Slider.SELECTORS.image_slide_container).animate(
+            { left: -748 },
+            70,
+            function () {
+              $(".image-slide-container img:first-child").appendTo(
+                $(Slider.SELECTORS.image_slide_container)
+              );
+              $(Slider.SELECTORS.image_slide_container).css("left", "");
             }
-            if (selectIndex < Slider.GLOBAL.index) {
-                let subIndex = Number(Slider.GLOBAL.index) - Number(selectIndex)
-                for (let i = 0; i < subIndex; i++) {
-                    $(Slider.SELECTORS.image_slide_container).animate({left: + 748},700, function() {
-                        $(".image-slide-container img:last-child").prependTo($(Slider.SELECTORS.image_slide_container));
-                        $(Slider.SELECTORS.image_slide_container).css("left","");
-                    })
-                    $(Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`).removeClass("active");
-                    Slider.GLOBAL.index = Slider.GLOBAL.index <= 0 ? Number(Slider.GLOBAL.imgCount)-1 : Number(Slider.GLOBAL.index)-1;
-                    $(Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`).addClass("active");
-                }
-            }
-        })
-    },
-  
-    setUpEvent: function () {
-        $(Slider.SELECTORS.prev_button).on("click", function() {
-            $(Slider.SELECTORS.image_slide_container).animate({left: + 748},700, function() {
-                $(".image-slide-container img:last-child").prependTo($(Slider.SELECTORS.image_slide_container));
-                $(Slider.SELECTORS.image_slide_container).css("left","");
-            })
-            $(Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`).removeClass("active");
-            Slider.GLOBAL.index = Slider.GLOBAL.index <= 0 ? Number(Slider.GLOBAL.imgCount)-1 : Number(Slider.GLOBAL.index)-1;
-            $(Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`).addClass("active");
-        });
-
-        $(Slider.SELECTORS.next_button).on("click", function() {
-            $(Slider.SELECTORS.image_slide_container).animate({left: - 748},700, function() {
-                $(".image-slide-container img:first-child").appendTo($(Slider.SELECTORS.image_slide_container));
-                $(Slider.SELECTORS.image_slide_container).css("left","");
-            })
-            $(Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`).removeClass("active");
-            Slider.GLOBAL.index = Slider.GLOBAL.index >= Number(Slider.GLOBAL.imgCount)-1 ? 0 : Number(Slider.GLOBAL.index)+1;
-            $(Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`).addClass("active");
-        });
-    },
-
-    renderContainer: function () {
-        var html = "";
-        for(let i = 0; i < Slider.GLOBAL.imgCount; i++) {
-            html += `<div class="select-button ${i===0 ? "active" : ""}"  index=${i}></div>`;
+          );
+          $(".image-slide-container img:first-child").appendTo(
+            $(Slider.SELECTORS.image_slide_container)
+          );
+          $(
+            Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+          ).removeClass("active");
+          Slider.GLOBAL.index =
+            Slider.GLOBAL.index >= Number(Slider.GLOBAL.imgCount) - 1
+              ? 0
+              : Number(Slider.GLOBAL.index) + 1;
+          $(
+            Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+          ).addClass("active");
         }
+      }
+      if (selectIndex < Slider.GLOBAL.index) {
+        let subIndex = Number(Slider.GLOBAL.index) - Number(selectIndex);
+        for (let i = 0; i < subIndex; i++) {
+          $(Slider.SELECTORS.image_slide_container).animate(
+            { left: +748 },
+            70,
+            function () {
+              $(".image-slide-container img:last-child").prependTo(
+                $(Slider.SELECTORS.image_slide_container)
+              );
+              $(Slider.SELECTORS.image_slide_container).css("left", "");
+            }
+          );
+          $(
+            Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+          ).removeClass("active");
+          Slider.GLOBAL.index =
+            Slider.GLOBAL.index <= 0
+              ? Number(Slider.GLOBAL.imgCount) - 1
+              : Number(Slider.GLOBAL.index) - 1;
+          $(
+            Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+          ).addClass("active");
+        }
+      }
+    });
+  },
 
-        $(Slider.SELECTORS.slide_select_container).html(html);
+  setUpEvent: function () {
+    $(Slider.SELECTORS.prev_button).on("click", function () {
+      $(Slider.SELECTORS.image_slide_container).animate(
+        { left: +748 },
+        700,
+        function () {
+          $(".image-slide-container img:last-child").prependTo(
+            $(Slider.SELECTORS.image_slide_container)
+          );
+          $(Slider.SELECTORS.image_slide_container).css("left", "");
+        }
+      );
+      $(
+        Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+      ).removeClass("active");
+      Slider.GLOBAL.index =
+        Slider.GLOBAL.index <= 0
+          ? Number(Slider.GLOBAL.imgCount) - 1
+          : Number(Slider.GLOBAL.index) - 1;
+      $(
+        Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+      ).addClass("active");
+    });
+
+    $(Slider.SELECTORS.next_button).on("click", function () {
+      $(Slider.SELECTORS.image_slide_container).animate(
+        { left: -748 },
+        700,
+        function () {
+          $(".image-slide-container img:first-child").appendTo(
+            $(Slider.SELECTORS.image_slide_container)
+          );
+          $(Slider.SELECTORS.image_slide_container).css("left", "");
+        }
+      );
+      $(
+        Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+      ).removeClass("active");
+      Slider.GLOBAL.index =
+        Slider.GLOBAL.index >= Number(Slider.GLOBAL.imgCount) - 1
+          ? 0
+          : Number(Slider.GLOBAL.index) + 1;
+      $(
+        Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+      ).addClass("active");
+    });
+  },
+
+  renderContainer: function () {
+    var html = "";
+    for (let i = 0; i < Slider.GLOBAL.imgCount; i++) {
+      html += `<div class="select-button ${
+        i === 0 ? "active" : ""
+      }"  index=${i}></div>`;
     }
 
+    $(Slider.SELECTORS.slide_select_container).html(html);
+  },
 
-  
-    // renderImgContainer: function () {
-    //   var html = "";
+  // renderImgContainer: function () {
+  //   var html = "";
 
-    //   for(let i = 0; i < Slider.GLOBAL.img.length; i++) {
-    //     html += `<img src="${Slider.GLOBAL.img[i]}" index="${i}">`
-    //   };
-    //   $(Slider.SELECTORS.image_slide_container).html(html);
-    // },
-  };
-  
-  $(document).ready(function () {
-    Slider.init();
-  });
+  //   for(let i = 0; i < Slider.GLOBAL.img.length; i++) {
+  //     html += `<img src="${Slider.GLOBAL.img[i]}" index="${i}">`
+  //   };
+  //   $(Slider.SELECTORS.image_slide_container).html(html);
+  // },
+};
+
+$(document).ready(function () {
+  Slider.init();
+});
--- index2.js
+++ index2.js
@@ -1,96 +1,144 @@
 var Slider = {
-    GLOBAL: {
-        imgCount: $(".image-slide-container img").length,
-        index: 0,
-        imgArray: [],
-    },
+  GLOBAL: {
+    imgCount: $(".image-slide-container img").length,
+    index: 0,
+    imgArray: [],
+  },
 
-    CONSTS: {},
+  CONSTS: {},
 
-    SELECTORS: {
-        image_slide_container: ".image-slide-container",
-        prev_button: ".prev-button",
-        next_button: ".next-button",
-        img: ".image-slide-container img",
-        slide_select_container: ".slide-select-container",
-        select_button: ".slide-select-container .select-button",
-    },
+  SELECTORS: {
+    image_slide_container: ".image-slide-container",
+    prev_button: ".prev-button",
+    next_button: ".next-button",
+    img: ".image-slide-container img",
+    slide_select_container: ".slide-select-container",
+    select_button: ".slide-select-container .select-button",
+  },
 
-    init: function() {
-        Slider.takeImg();
-        Slider.renderContainer();
-        Slider.buttonEvent();
-        Slider.selectEvent();
-    },
+  init: function () {
+    Slider.takeImg();
+    Slider.renderContainer();
+    Slider.buttonEvent();
+    Slider.selectEvent();
+  },
 
-    takeImg: function() {
-        for (let i = 0; i < Slider.GLOBAL.imgCount; i++) {
-            imgInfo = {img:$(Slider.SELECTORS.img + `[index=${i}]`).attr("src"),
-                    name:$(Slider.SELECTORS.img + `[index=${i}]`).attr("alt")};
+  takeImg: function () {
+    for (let i = 0; i < Slider.GLOBAL.imgCount; i++) {
+      imgInfo = {
+        img: $(Slider.SELECTORS.img + `[index=${i}]`).attr("src"),
+        name: $(Slider.SELECTORS.img + `[index=${i}]`).attr("alt"),
+      };
 
-            Slider.GLOBAL.imgArray[i] = imgInfo;
+      Slider.GLOBAL.imgArray[i] = imgInfo;
+    }
+  },
+
+  selectEvent: function () {
+    $(Slider.SELECTORS.select_button).on("click", function () {
+      let selectIndex = $(this).attr("index");
+      if (Number(selectIndex) > Slider.GLOBAL.index) {
+        $(
+          Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+        ).removeClass("active");
+        $(Slider.SELECTORS.select_button + `[index=${selectIndex}]`).addClass(
+          "active"
+        );
+        Slider.goRight(Slider.GLOBAL.index, selectIndex);
+      }
+
+      if (Number(selectIndex) < Slider.GLOBAL.index) {
+        $(
+          Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+        ).removeClass("active");
+        $(Slider.SELECTORS.select_button + `[index=${selectIndex}]`).addClass(
+          "active"
+        );
+        Slider.goLeft(Slider.GLOBAL.index, selectIndex);
+      }
+    });
+  },
+
+  buttonEvent: function () {
+    $(Slider.SELECTORS.next_button).on("click", function () {
+      $(
+        Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+      ).removeClass("active");
+      Slider.goRight(Slider.GLOBAL.index, Number(Slider.GLOBAL.index) + 1);
+
+      $(
+        Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+      ).addClass("active");
+    });
+    $(Slider.SELECTORS.prev_button).on("click", function () {
+      $(
+        Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+      ).removeClass("active");
+      Slider.goLeft(Slider.GLOBAL.index, Number(Slider.GLOBAL.index) - 1);
+
+      $(
+        Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`
+      ).addClass("active");
+    });
+  },
+
+  goRight: function (currentIndex, nextIndex) {
+    let renderCount = nextIndex - currentIndex;
+    leftChange = Number(renderCount) * 748;
+    $(Slider.SELECTORS.image_slide_container).animate(
+      { left: -Number(leftChange) },
+      700,
+      function () {
+        for (let i = 0; i < renderCount; i++) {
+          $(".image-slide-container img:first-child").appendTo(
+            $(Slider.SELECTORS.image_slide_container)
+          );
+          $(Slider.SELECTORS.image_slide_container).css("left", "");
         }
-    },
+      }
+    );
+    if (nextIndex >= Number(Slider.GLOBAL.imgCount)) {
+      Slider.GLOBAL.index = 0;
+    } else Slider.GLOBAL.index = nextIndex;
+  },
 
-    selectEvent: function() {
-        $(Slider.SELECTORS.select_button).on("click", function(){
-            let selectIndex = $(this).attr("index");
-            if (Number(selectIndex) > Slider.GLOBAL.index) Slider.goRight(Slider.GLOBAL.index, selectIndex);
-            if (Number(selectIndex) < Slider.GLOBAL.index) Slider.goLeft(Slider.GLOBAL.index, selectIndex);
-        })
-        
-        
-    },
-
-    buttonEvent: function() {
-        $(Slider.SELECTORS.next_button).on("click", function() {
-            Slider.goRight(Slider.GLOBAL.index, Number(Slider.GLOBAL.index)+1)
-        });
-        $(Slider.SELECTORS.prev_button).on("click", function() {
-            Slider.goLeft(Slider.GLOBAL.index, Number(Slider.GLOBAL.index)-1)
-        });
-    },
-
-    goRight: function(currentIndex, nextIndex) {
-        let renderCount = nextIndex - currentIndex;
-        leftChange = Number(renderCount)*748;
-        $(Slider.SELECTORS.image_slide_container).animate({left: - Number(leftChange)},700, function() {
-            for (let i = 0; i < renderCount; i++) {
-                $(".image-slide-container img:first-child").appendTo($(Slider.SELECTORS.image_slide_container));
-                $(Slider.SELECTORS.image_slide_container).css("left","");
-            }
-        })
-        if (nextIndex >= Number(Slider.GLOBAL.imgCount)) {
-            Slider.GLOBAL.index = 0;
+  goLeft: function (currentIndex, nextIndex) {
+    let renderCount = currentIndex - nextIndex;
+    leftChange = Number(renderCount) * 748;
+    for (let i = 0; i < renderCount; i++) {
+      $(".image-slide-container img:last-of-type").prependTo(
+        $(Slider.SELECTORS.image_slide_container)
+      );
+    }
+    $(Slider.SELECTORS.image_slide_container).css(
+      "left",
+      -748 * renderCount + "px"
+    );
+    $(Slider.SELECTORS.image_slide_container).animate(
+      { left: 0 },
+      700,
+      function () {
+        for (let i = 0; i < renderCount; i++) {
+          $(Slider.SELECTORS.image_slide_container).css("left", "");
         }
-        else Slider.GLOBAL.index = nextIndex;
-    },
+      }
+    );
+    if (nextIndex < 0) {
+      Slider.GLOBAL.index = Number(Slider.GLOBAL.imgCount) - 1;
+    } else Slider.GLOBAL.index = nextIndex;
+  },
 
-    goLeft: function(currentIndex, nextIndex) {
-        let renderCount = currentIndex - nextIndex;
-        leftChange = Number(renderCount)*748;
-        $(Slider.SELECTORS.image_slide_container).animate({left: + Number(leftChange)},700, function() {
-            for (let i = 0; i < renderCount; i++) {
-                $(".image-slide-container img:last-child").prependTo($(Slider.SELECTORS.image_slide_container));
-                $(Slider.SELECTORS.image_slide_container).css("left","");
-            }
-        })
-        if (nextIndex < 0) {
-            Slider.GLOBAL.index = Number(Slider.GLOBAL.imgCount)-1;
-        }
-        else Slider.GLOBAL.index = nextIndex;
-    },
-
-    renderContainer: function () {
-        var html = "";
-        for(let i = 0; i < Slider.GLOBAL.imgCount; i++) {
-            html += `<div class="select-button ${i===0 ? "active" : ""}"  index=${i}></div>`;
-        }
-
-        $(Slider.SELECTORS.slide_select_container).html(html);
+  renderContainer: function () {
+    var html = "";
+    for (let i = 0; i < Slider.GLOBAL.imgCount; i++) {
+      html += `<div class="select-button ${
+        i === 0 ? "active" : ""
+      }"  index=${i}></div>`;
     }
 
-}
-$(document).ready(function() {
-    Slider.init();
+    $(Slider.SELECTORS.slide_select_container).html(html);
+  },
+};
+$(document).ready(function () {
+  Slider.init();
 });
