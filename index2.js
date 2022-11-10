a4795154149baa4072565e35d4d4f303144bf7b1
artoursong <duy147862@gmail.com>
2022-11-10 17:09:43 +0700

    update rm 2

--- index2.js
+++ index2.js
@@ -21,50 +21,38 @@ var Slider = {
   dotEvent: function () {
     $(Slider.SELECTORS.select_button).on("click", function () {
       let selectIndex = $(this).attr("index");
-      if (Number(selectIndex) > Slider.GLOBAL.index) {
-        Slider.goRight(Slider.GLOBAL.index, selectIndex);
-        Slider.dotActive(Slider.GLOBAL.index, selectIndex);
-      }
-      if (Number(selectIndex) < Slider.GLOBAL.index) {
-        Slider.goLeft(Slider.GLOBAL.index, selectIndex);
-        Slider.dotActive(Slider.GLOBAL.index, selectIndex);   
-      }
+      if (Number(selectIndex) > Slider.GLOBAL.index) Slider.moveImg(Slider.GLOBAL.index, selectIndex);
+      if (Number(selectIndex) < Slider.GLOBAL.index) Slider.moveImg(Slider.GLOBAL.index, selectIndex);  
     });
   },
   dotActive: function (currentIndex, nextIndex) {
-    Slider.GLOBAL.index = nextIndex;
-    if(nextIndex >= Slider.GLOBAL.imgCount) Slider.GLOBAL.index = 0
-    if(nextIndex < 0) Slider.GLOBAL.index = Slider.GLOBAL.imgCount - 1;
+    Slider.GLOBAL.index = nextIndex >= Slider.GLOBAL.imgCount ? 0 : nextIndex < 0 ? Slider.GLOBAL.imgCount - 1 : nextIndex;
     $(Slider.SELECTORS.select_button + `[index=${currentIndex}]`).removeClass("active");
     $(Slider.SELECTORS.select_button + `[index=${Slider.GLOBAL.index}]`).addClass("active");
   },
   buttonEvent: function () {
     $(Slider.SELECTORS.next_button).on("click", function () {
-      let nextIndex = Number(Slider.GLOBAL.index) + 1;
-      Slider.goRight(Slider.GLOBAL.index, nextIndex);
-      Slider.dotActive(Slider.GLOBAL.index, nextIndex);
+      Slider.moveImg(Slider.GLOBAL.index, Number(Slider.GLOBAL.index) + 1);
     });
     $(Slider.SELECTORS.prev_button).on("click", function () {
-      let nextIndex = Number(Slider.GLOBAL.index) - 1;
-      Slider.goLeft(Slider.GLOBAL.index, Number(Slider.GLOBAL.index) - 1);
-      Slider.dotActive(Slider.GLOBAL.index, nextIndex);
+      Slider.moveImg(Slider.GLOBAL.index, Number(Slider.GLOBAL.index) - 1);;
     });
   },
-  goRight: function (currentIndex, nextIndex) {
+  moveImg: function (currentIndex, nextIndex) {
     let renderCount = nextIndex - currentIndex;
     leftChange = Number(renderCount) * Slider.GLOBAL.imgWidth;
-    $(Slider.SELECTORS.image_slide_container).animate({ left: -Number(leftChange) },700,function () {
-        for (let i = 0; i < renderCount; i++) $(".image-slide-container img:first-child").appendTo($(Slider.SELECTORS.image_slide_container));
+    if (renderCount > 0) {
+      $(Slider.SELECTORS.image_slide_container).animate({ left: -Number(leftChange) },700,function () {
+        for (let i = 0; i < renderCount; i++) $(".image-slide-container img:first-of-type").appendTo($(Slider.SELECTORS.image_slide_container));
         $(Slider.SELECTORS.image_slide_container).css("left", 0);
-      }
-    );
-  },
-  goLeft: function (currentIndex, nextIndex) {
-    let renderCount = currentIndex - nextIndex;
-    leftChange = Number(renderCount) * Slider.GLOBAL.imgWidth;
-    for (let i = 0; i < renderCount; i++) $(".image-slide-container img:last-of-type").prependTo($(Slider.SELECTORS.image_slide_container));
-    $(Slider.SELECTORS.image_slide_container).css("left",-Slider.GLOBAL.imgWidth * renderCount + "px");
-    $(Slider.SELECTORS.image_slide_container).animate({ left: 0 },700,function () {});
+      });
+    }
+    if (renderCount <0) {
+      for (let i = 0; i < -renderCount; i++) $(".image-slide-container img:last-of-type").prependTo($(Slider.SELECTORS.image_slide_container));
+      $(Slider.SELECTORS.image_slide_container).css("left",Slider.GLOBAL.imgWidth * renderCount + "px");
+      $(Slider.SELECTORS.image_slide_container).animate({ left: 0 },700,function () {});
+    }
+    Slider.dotActive(Slider.GLOBAL.index, nextIndex);
   },
   renderDot: function () {
     var html = "";
